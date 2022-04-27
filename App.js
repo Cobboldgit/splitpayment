f5c4df782c61417003724281efef2b70b2f5d25d
Cobboldgit <augustinecobbold6@gmail.com>
2022-04-27 00:23:03 +0000

    fiix errors

--- App.js
+++ App.js
@@ -15,14 +15,16 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { appColor } from "./constants";
 
 // ignore yellow errors
-LogBox.ignoreLogs([
-  "Setting a timer for a long period of time, i.e. multiple minutes, is a performance and correctness issue on Android as it keeps the timer module awake, and timers can only be called when the app is in the foreground. See https://github.com/facebook/react-native/issues/12981 for more info.",
-  "AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-async-storage/async-storage' instead of 'react-native'. See https://github.com/react-native-async-storage/async-storage",
-  "Cannot connect to Metro.",
-  "[2022-04-23T21:18:07.589Z]  @firebase/firestore:, Firestore (8.10.1)",
-  "Unhandled promise rejection",
-  "Unable to activate keep awake"
-]);
+// LogBox.ignoreLogs([
+//   "Setting a timer for a long period of time, i.e. multiple minutes, is a performance and correctness issue on Android as it keeps the timer module awake, and timers can only be called when the app is in the foreground. See https://github.com/facebook/react-native/issues/12981 for more info.",
+//   "AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-async-storage/async-storage' instead of 'react-native'. See https://github.com/react-native-async-storage/async-storage",
+//   "Cannot connect to Metro.",
+//   "[2022-04-23T21:18:07.589Z]  @firebase/firestore:, Firestore (8.10.1)",
+//   "Unhandled promise rejection",
+//   "Unable to activate keep awake"
+// ]);
+
+LogBox.ignoreAllLogs()
 
 // to make the animations work on android
 if (
